cb1d6653e02a34a77e3aed4ded7b89cd791c3cae
Hansol Lee <leehansol1993@gmail.com>
2022-10-27 22:54:29 -0700

    added login page

---
+++ TrainingApp/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingApp.Controllers
+{
+    public class AccountController: Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public AccountController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
