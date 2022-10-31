ad6404255421ee8c12ea4f05c315e1f178f65984
Hansol Lee <leehansol1993@gmail.com>
2022-10-31 01:30:42 -0700

    set sqldb

---
+++ TrainingAppAPI/DTOs/TaskDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TrainingAppAPI.DTOs
+{
+    public class TaskDTO
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+    }
+}
+
---
+++ TrainingAppAPI/DTOs/UserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TrainingAppAPI.DTOs
+{
+    public class UserDTO
+    {
+        public Guid ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public string UserCode { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int State { get; set; }
+
+        public int CompanyID { get; set; }
+        public CompanyDTO Company { get; set; }
+    }
+}
+
