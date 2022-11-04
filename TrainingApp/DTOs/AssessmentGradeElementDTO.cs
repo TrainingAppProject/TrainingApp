8cb99f9ca5ffa684828b76785ad2648eb3fde097
Hansol Lee <leehansol1993@gmail.com>
2022-11-03 23:51:47 -0700

    Finalized db.

---
+++ TrainingApp/DTOs/AssessmentGradeElementDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TrainingApp.DTOs
+{
+    public class AssessmentGradeElementDTO
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public bool IsFail { get; set; }
+
+        public Guid AssessmentGradeID { get; set; }
+        public AssessmentGradeDTO AssessmentGrade { get; set; }
+    }
+}
+
---
+++ TrainingApp/Migrations/20221104064747_InitialCreate.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainingApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assessments",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompanyID = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OverallGrade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsTaskMandatory = table.Column<bool>(type: "bit", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false),
+                    ExaminerID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExaminerSigned = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TraineeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TraineeSigned = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TemplateID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assessments", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<bool>(type: "bit", nullable: false),
+                    GradeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssessmentElements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderNo = table.Column<int>(type: "int", nullable: false),
+                    AssessmentID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TemplateElementID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AssessmentTaskID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AssessmentGradeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssessmentElements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssessmentElements_Assessments_AssessmentID",
+                        column: x => x.AssessmentID,
+                        principalTable: "Assessments",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false),
+                    CompanyID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Users_Companies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Companies",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GradeElements",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsFail = table.Column<bool>(type: "bit", nullable: false),
+                    GradeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GradeElements", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_GradeElements_Grades_GradeID",
+                        column: x => x.GradeID,
+                        principalTable: "Grades",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Templates",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedID = table.Column<int>(type: "int", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsTaskMandatory = table.Column<bool>(type: "bit", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false),
+                    ScriptNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyID = table.Column<int>(type: "int", nullable: false),
+                    GradeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Templates", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Templates_Companies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Companies",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Templates_Grades_GradeID",
+                        column: x => x.GradeID,
+                        principalTable: "Grades",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssessmentGrades",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false),
+                    AssessmentElementID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssessmentGrades", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_AssessmentGrades_AssessmentElements_AssessmentElementID",
+                        column: x => x.AssessmentElementID,
+                        principalTable: "AssessmentElements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssessmentTasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AssessmentElementID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssessmentTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssessmentTasks_AssessmentElements_AssessmentElementID",
+                        column: x => x.AssessmentElementID,
+                        principalTable: "AssessmentElements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TemplateElements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderNo = table.Column<int>(type: "int", nullable: false),
+                    TemplateID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TaskID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemplateElements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TemplateElements_Tasks_TaskID",
+                        column: x => x.TaskID,
+                        principalTable: "Tasks",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TemplateElements_Templates_TemplateID",
+                        column: x => x.TemplateID,
+                        principalTable: "Templates",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssessmentGradeElements",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsFail = table.Column<bool>(type: "bit", nullable: false),
+                    AssessmentGradeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssessmentGradeElements", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_AssessmentGradeElements_AssessmentGrades_AssessmentGradeID",
+                        column: x => x.AssessmentGradeID,
+                        principalTable: "AssessmentGrades",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssessmentElements_AssessmentID",
+                table: "AssessmentElements",
+                column: "AssessmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssessmentGradeElements_AssessmentGradeID",
+                table: "AssessmentGradeElements",
+                column: "AssessmentGradeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssessmentGrades_AssessmentElementID",
+                table: "AssessmentGrades",
+                column: "AssessmentElementID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssessmentTasks_AssessmentElementID",
+                table: "AssessmentTasks",
+                column: "AssessmentElementID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GradeElements_GradeID",
+                table: "GradeElements",
+                column: "GradeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemplateElements_TaskID",
+                table: "TemplateElements",
+                column: "TaskID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemplateElements_TemplateID",
+                table: "TemplateElements",
+                column: "TemplateID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Templates_CompanyID",
+                table: "Templates",
+                column: "CompanyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Templates_GradeID",
+                table: "Templates",
+                column: "GradeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CompanyID",
+                table: "Users",
+                column: "CompanyID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssessmentGradeElements");
+
+            migrationBuilder.DropTable(
+                name: "AssessmentTasks");
+
+            migrationBuilder.DropTable(
+                name: "GradeElements");
+
+            migrationBuilder.DropTable(
+                name: "TemplateElements");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "AssessmentGrades");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Templates");
+
+            migrationBuilder.DropTable(
+                name: "AssessmentElements");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "Grades");
+
+            migrationBuilder.DropTable(
+                name: "Assessments");
+        }
+    }
+}
